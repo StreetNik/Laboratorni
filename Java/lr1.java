7052d32fe24a6b0e43b9c13b7de605f0f2c39a50
StreetNik <nikulicastas2004@gmail.com>
2022-02-09 13:30:53 +0200

    update

--- Java/lr1.java
+++ Java/lr1.java
@@ -1,7 +1,35 @@
 package com.company;
+import java.util.Scanner;
 
-public class lr1{
-    public static void main(String[] args){
-        System.out.printLn("Hello World");
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Введіть номер завдання: ");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        switch(n){
+            case 1:
+                //zavdannia 1
+                System.out.println("Завдання 1: ");
+                System.out.println("Введіть спосіб вирішення завдання(1 або 2): ");
+                Scanner in = new Scanner(System.in);
+
+                break;
+            case 2:
+                //zavdannia 2
+                System.out.println("Завдання 2: ");
+                break;
+            case 3:
+                //zavdannia 3
+                System.out.println("Завдання 3: ");
+                break;
+            case 4:
+                //zavdannia 4
+                System.out.println("Завдання 4: ");
+                break;
+            default:
+                System.out.println("Номер завдання введено не вірно або такого завдання не існує");
+                break;
+        }
     }
 }
